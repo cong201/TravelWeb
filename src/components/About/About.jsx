6ec4d6b462502719b93fe272aea0bc0776a1819c
Công Lê <levancong_t64@gmail.com>
2023-07-10 02:53:25 +0700

    update offer , about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,69 @@
 import React from "react";
 import './about.css'
 
-function About() {
+import img1 from '../../assets/hikings.jpg'
+import img2 from '../../assets/message.jpg'
+import img3 from '../../assets/user.jpg'
+import img4 from '../../assets/moutain.jpg'
+
+import video from '../../assets/video.mp4'
+
+const About = () => {
     return (
-        <div></div>
+        <section className="about section">
+            <div className="secContainer">
+                <h3 className="title">Why Hikings?</h3>
+
+                <div className="mainContent container grid">
+                    <div className="singleItem">
+                        <img src={img4} alt="Image Item" />
+                        <h3>100+ Moutain</h3>
+                        <p>
+                            Research shows that a chance to break away from the normal rhymology of daily life reduces stress and improving
+                        </p>
+                    </div>
+
+                    <div className="singleItem">
+                        <img src={img1} alt="Image Item" />
+                        <h3>300+ Hikings</h3>
+                        <p>
+                            Research shows that a chance to break away from the normal rhymology of daily life reduces stress and improving
+                        </p>
+                    </div>
+
+                    <div className="singleItem">
+                        <img src={img3} alt="Image Item" />
+                        <h3>100+ Customers</h3>
+                        <p>
+                            Research shows that a chance to break away from the normal rhymology of daily life reduces stress and improving
+                        </p>
+                    </div>
+
+                    <div className="singleItem">
+                        <img src={img2} alt="Image Item" />
+                        <h3>150+ message</h3>
+                        <p>
+                            Research shows that a chance to break away from the normal rhymology of daily life reduces stress and improving
+                        </p>
+                    </div>
+                </div>
+
+                <div className="videoCard container">
+                    <div className="cardContent grid">
+
+                        <div className="cardText">
+                            <h2>Wonderfull hourse in here</h2>
+                            <p>The advanture subranking is base on a equally weight average of scores from 100 countries</p>
+                        </div>
+
+                        <div className="cardVideo">
+                            <video src={video} autoPlay loop muted
+                                type="video/mp4"></video>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,10 +1,110 @@
 import React from "react";
 import './offer.css'
+import { MdKingBed, MdBathtub, MdAirportShuttle, MdLocationOn } from 'react-icons/md'
+import { FaWifi } from 'react-icons/fa'
+import { BsArrowRightShort } from 'react-icons/bs'
+import img1 from '../../assets/picture_5.jpg'
+import img2 from '../../assets/picture_7.jpg'
+import img3 from '../../assets/picture_8.jpg'
+import img4 from '../../assets/picture_1.jpg'
+import img5 from '../../assets/picture_3.jpg'
 
-function Offer() {
-    return (<div>
+const Offers = [
+    {
+        id: 1,
+        imgSrc: img1,
+        destTitle: 'Cua Lo',
+        location: '450 Vine #310, Nghe An',
+        price: '$400'
+    },
+    {
+        id: 2,
+        imgSrc: img2,
+        destTitle: 'Nha tho lon',
+        location: 'Ha Noi',
+        grade: '50 Vine #60,$100'
+    },
+    {
+        id: 3,
+        imgSrc: img3,
+        destTitle: 'Do Luong',
+        location: '40 Vine #10, Nghe An',
+        grade: '$150'
+    },
+    {
+        id: 4,
+        imgSrc: img4,
+        destTitle: 'Quang Son',
+        location: '300 Vine #60,Do Luong',
+        grade: '$1000'
+    },
+    {
+        id: 5,
+        imgSrc: img5,
+        destTitle: 'Vinh City',
+        location: '140 Vine #110,Nghe An',
+        grade: '$868'
+    },
+]
 
-    </div>);
+const Offer = () => {
+    return (
+        <section className="offer container section">
+            <div className="secContainer">
+                <div className="secIntro">
+                    <h2 className="secTitle">Special offer</h2>
+                    <p>From historical cities to natural spectecular , come see the best place of the world</p>
+                </div>
+                <div className="mainContent grid">
+                    {
+                        Offers.map(({ id, imgSrc, destTitle, location, price }) => {
+                            return (<div className="singleOffer">
+                                <div className="destImg">
+                                    <img src={imgSrc} alt="Image Name" />
+                                    <span className="discount">30% off</span>
+                                </div>
+                                <div className="offerBody">
+                                    <div className="price flex">
+                                        <h4>{price}</h4>
+                                        <span className="status">For Rent</span>
+                                    </div>
+
+                                    <div className="amenities flex">
+                                        <div className="singleAmen flex">
+                                            <MdKingBed className="icon" />
+                                            <small>2 Beds</small>
+                                        </div>
+                                        <div className="singleAmen flex">
+                                            <MdBathtub className="icon" />
+                                            <small>1 Bath</small>
+                                        </div>
+                                        <div className="singleAmen flex">
+                                            <FaWifi className="icon" />
+                                            <small>Wi-fi</small>
+                                        </div>
+                                        <div className="singleAmen flex">
+                                            <MdAirportShuttle className="icon" />
+                                            <small>Airport</small>
+                                        </div>
+                                    </div>
+
+                                    <div className="location flex">
+                                        <MdLocationOn className="icon" />
+                                        <small>{location}</small>
+                                    </div>
+
+                                    <button className="btn flex">
+                                        View Detail
+                                        <BsArrowRightShort className="icon" />
+                                    </button>
+                                </div>
+                            </div>)
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Offer;
